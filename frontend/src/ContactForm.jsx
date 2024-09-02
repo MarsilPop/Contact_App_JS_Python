b034d72234366e53e2347a396ee2160f5edc64e4
Marcell <149784148+MarsilPop@users.noreply.github.com>
2024-09-02 15:55:55 +0200

    create_contacts, css

--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -13,7 +13,7 @@ const ContactForm = ({}) => {
             lastName,
             email,
         };
-        const url = "http://127.0.0.1:5000/create_contact";
+        const url = "http://127.0.0.1:5000/create_contacts";
         const options = {
             method: "POST",
             headers: {
